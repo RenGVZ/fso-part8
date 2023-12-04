a4cdd6aa9f37e52c166302594b25677b562a9315
Gavin Garza <gaving@live.com>
2023-12-04 21:03:56 +0900

    added apollo client to work with FE requests

---
+++ 1-example/frontend/src/main.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query({ query }).then((res) => console.log(res.data))
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+)
